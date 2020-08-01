4771335e761b5c8a75f4d593263e551ec0bf8bce
Alan Chang <alanchan@Alans-MacBook-Pro.local>
2020-08-01 14:44:14 -0700

    playing around with blog format

--- init.js
+++ init.js
@@ -12,19 +12,59 @@
       let top = rect.top + scrollTop;
       let bottom = top + rect.height;
       let id = sections[i].id;
+      let stickyTags = sections[i].querySelector('.tags');
+      let contentOffset = 100;
+      scrollTop += contentOffset;
 
-      if ((i === 0 && scrollTop < top) ||
-          (i === sections.length - 1 && scrollTop > bottom) ||
-          (scrollTop > top && scrollTop < bottom)) {
+      // console.log(i, scrollTop, top);
+
+      if (i === 0) {
+        progress.style.height = '100%';
+        indicator.style.transform = '';
+
+      } else if (scrollTop < top) {
+        stickyTags.classList.remove('sticky');
+        stickyTags.style = null;
+
+      } else if (scrollTop > top && scrollTop < bottom) {
         let perc = Math.min(Math.max((scrollTop - top) / rect.height, 0), 1);
-        console.log("ACTIVE", i, perc);
+        // console.log("ACTIVE", i, perc);
         progress.style.height = perc * 100 + '%';
+        indicator.style.transform = `translateY(${i * 48}px)`;
+        if (stickyTags) {
+          let tagsRect = stickyTags.getBoundingClientRect();
+          if (scrollTop > bottom - tagsRect.height) {
+            if (!stickyTags.classList.contains('sticky-bottom')) {
+              console.log("STICKY BOTTOM", scrollTop,  bottom, tagsRect.height);
+              stickyTags.classList.remove('sticky');
+              stickyTags.classList.add('sticky-bottom');
+              stickyTags.style = null;
+              stickyTags.style.top = 'auto';
+              stickyTags.style.bottom = 0;
+            }
+          } else if (!stickyTags.classList.contains('sticky')) {
+            stickyTags.classList.remove('sticky-bottom');
+            stickyTags.classList.add('sticky');
+            console.log("STICKY", `${tagsRect.left + tagsRect.width}px`);
+            stickyTags.style = null;
+            stickyTags.style.position = 'fixed';
+            stickyTags.style.transform = 'translateX(0)';
+            stickyTags.style.top = contentOffset + 'px';
+            stickyTags.style.right = 'auto';
+            stickyTags.style.left = `${tagsRect.left}px`;
+          }
+
+        }
+
+
+      } else if (i === sections.length - 1 && scrollTop > bottom) {
+        progress.style.height = '100%';
         indicator.style.transform = `translateY(${i * 48}px)`;
 
       }
-      console.log(top, bottom);
+      // console.log(top, bottom);
     }
-    console.log(scrollTop);
+    // console.log(scrollTop);
   }
 
   handleScroll();
