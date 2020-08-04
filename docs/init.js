081ff193c51af0d77fd0b52fbc48ad911566bdd9
Alan Chang <alanchan@Alans-MacBook-Pro.local>
2020-08-04 10:47:51 -0700

    fixes and responsiveness

--- docs/init.js
+++ docs/init.js
@@ -23,61 +23,63 @@
       let link = document.querySelector('.section-link[data-target='+id+']');
       scrollTop += contentOffset;
 
+      if (window.innerWidth <= 700) {
+        indicator.style = null;
 
-      if (i === 0) {
-        // progress.style.height = '100%';
-        indicator.style.transform = '';
-        for (let i = 0; i < links.length; i++) { links[i].classList.remove('active') }
+      } else {
+        if (i === 0) {
+          // progress.style.height = '100%';
+          indicator.style.transform = '';
+          for (let i = 0; i < links.length; i++) { links[i].classList.remove('active') }
 
-      } else if (scrollTop < top) {
-        stickyTags.classList.remove('sticky');
-        stickyTags.style = null;
+        } else if (scrollTop < top) {
+          stickyTags.classList.remove('sticky');
+          stickyTags.style = null;
 
-      } else if (i === sections.length - 1 &&
-                 (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        // progress.style.height = '100%';
-        if (!link.classList.contains('active')) {
-          indicator.style.transform = `translateY(${i * 36}px)`;
-          link.classList.add('active');
-        }
+        } else if (i === sections.length - 1 &&
+                   (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+          // progress.style.height = '100%';
+          if (!link.classList.contains('active')) {
+            indicator.style.transform = `translateY(${i * 36}px)`;
+            link.classList.add('active');
+          }
 
-      } else if (scrollTop > top && scrollTop < bottom) {
-        let perc = Math.min(Math.max((scrollTop - top) / rect.height, 0), 1);
-        // console.log("ACTIVE", i, perc);
-        // progress.style.height = perc * 100 + '%';
-        if (!link.classList.contains('active')) {
-          indicator.style.transform = `translateY(${i * 36}px)`;
-          link.classList.add('active');
-        }
-        if (stickyTags) {
-          let tagsRect = stickyTags.getBoundingClientRect();
-          if (scrollTop > bottom - tagsRect.height - contentOffset) {
-            if (!stickyTags.classList.contains('sticky-bottom')) {
-              // console.log("STICKY BOTTOM", scrollTop,  bottom, tagsRect.height);
-              stickyTags.classList.remove('sticky');
-              stickyTags.classList.add('sticky-bottom');
+        } else if (scrollTop > top && scrollTop < bottom) {
+          let perc = Math.min(Math.max((scrollTop - top) / rect.height, 0), 1);
+          // console.log("ACTIVE", i, perc);
+          // progress.style.height = perc * 100 + '%';
+          if (!link.classList.contains('active')) {
+            indicator.style.transform = `translateY(${i * 36}px)`;
+            link.classList.add('active');
+          }
+          if (stickyTags) {
+            let tagsRect = stickyTags.getBoundingClientRect();
+            if (scrollTop > bottom - tagsRect.height - contentOffset) {
+              if (!stickyTags.classList.contains('sticky-bottom')) {
+                // console.log("STICKY BOTTOM", scrollTop,  bottom, tagsRect.height);
+                stickyTags.classList.remove('sticky');
+                stickyTags.classList.add('sticky-bottom');
+                stickyTags.style = null;
+                stickyTags.style.top = 'auto';
+                stickyTags.style.bottom = contentOffset + 'px';
+              }
+            } else if (!stickyTags.classList.contains('sticky')) {
+              stickyTags.classList.remove('sticky-bottom');
+              stickyTags.classList.add('sticky');
+              // console.log("STICKY", `${tagsRect.left + tagsRect.width}px`);
               stickyTags.style = null;
-              stickyTags.style.top = 'auto';
-              stickyTags.style.bottom = contentOffset + 'px';
+              stickyTags.style.position = 'fixed';
+              stickyTags.style.transform = 'translateX(0)';
+              stickyTags.style.top = contentOffset + 'px';
+              stickyTags.style.right = 'auto';
+              stickyTags.style.left = `${tagsRect.left}px`;
             }
-          } else if (!stickyTags.classList.contains('sticky')) {
-            stickyTags.classList.remove('sticky-bottom');
-            stickyTags.classList.add('sticky');
-            // console.log("STICKY", `${tagsRect.left + tagsRect.width}px`);
-            stickyTags.style = null;
-            stickyTags.style.position = 'fixed';
-            stickyTags.style.transform = 'translateX(0)';
-            stickyTags.style.top = contentOffset + 'px';
-            stickyTags.style.right = 'auto';
-            stickyTags.style.left = `${tagsRect.left}px`;
+
           }
 
         }
-
       }
-      // console.log(top, bottom);
     }
-    // console.log(scrollTop);
   }
 
   handleScroll();
