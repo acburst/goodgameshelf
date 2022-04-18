7abda757ced603cd8ed69871818c7cda72c15a0a
Christian Kasilag <cjkasilag@gmail.com>
2022-04-18 15:23:16 -0400

    new players article added

--- pub.bat
+++ pub.bat
@@ -8,4 +8,5 @@ call echo %cd%\docs
 call move /Y %cd%/docs/recommendations\index.html %cd%\docs
 call move /Y %cd%/docs/recommendations\families.html %cd%\docs\best-games
 call move /Y %cd%/docs/recommendations\2-players.html %cd%\docs\best-games
+call move /Y %cd%/docs/recommendations\new-players.html %cd%\docs\best-games
 call %cd%\docs\index.html
